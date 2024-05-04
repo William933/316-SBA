95a710da075a83266871d7fa61694b3d5cc67821
William Allen <williamallen933@gmail.com>
2024-05-03 20:05:57 -0400

    adding the format for days and weekdays in css, strating a formula for javscript

---
+++ script.js
@@ -0,0 +1,8 @@
+const calendar = document.getElementById('calendar');
+
+function createCalendar(year, month) {
+  const daysInMonth = new Date(year, month + 1, 0).getDate();
+  const firstDayOfMonth = new Date(year, month, 1).getDay();
+  
+  const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
